e18efec36f892019aa42eaa84c95d188d345536e
Lourivaldo Vasconcelos <lourivaldo@mangue3.com.br>
2020-03-26 19:16:01 -0300

    Child Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,21 +9,37 @@ import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import {ROUTES} from './app.routes';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
+import {RestaurantComponent} from './restaurants/restaurant/restaurant.component';
+import {RestaurantsService} from './restaurants/restaurants.service';
+import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
+import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
+import { MenuComponent } from './restaurant-detail/menu/menu.component';
+import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
+import { ReviewsComponent } from './reviews/reviews.component';
 
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        HeaderComponent,
-        HomeComponent,
-        AboutComponent,
-    ],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    RestaurantsComponent,
+    RestaurantComponent,
+    RestaurantComponent,
+    RestaurantDetailComponent,
+    ShoppingCartComponent,
+    MenuComponent,
+    MenuItemComponent,
+    ReviewsComponent,
+  ],
   imports: [
     BrowserModule,
     HttpModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [],
+  providers: [RestaurantsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/restaurants/restaurants.service.ts
@@ -0,0 +1,26 @@
+import {Restaurant} from './restaurant/restaurant.model';
+import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
+import {MEAT_API} from '../app.api';
+import {Observable} from 'rxjs';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import {ErrorHandler} from '../app.error-handler';
+
+@Injectable()
+export class RestaurantsService {
+
+  constructor(private http: Http) {}
+
+  restaurants(): Observable<Restaurant[]> {
+    return this.http.get(`${MEAT_API}/restaurants`)
+      .map(response => response.json())
+      .catch(ErrorHandler.handlerError);
+  }
+
+  restaurantById(id: string): Observable<Restaurant> {
+    return this.http.get(`${MEAT_API}/restaurants/${id}`)
+      .map(response => response.json())
+      .catch(ErrorHandler.handlerError);
+  }
+}
