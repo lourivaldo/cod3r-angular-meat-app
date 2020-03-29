895a4ad93ad5bb913b70b68d1bee7948c16a6a87
Lourivaldo Vasconcelos <lourivaldo@mangue3.com.br>
2020-03-29 16:19:23 -0300

    Preload modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
+import {PreloadAllModules, RouterModule} from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import {ROUTES} from './app.routes';
@@ -39,7 +39,7 @@ import { OrderSummaryComponent } from './order-summary/order-summary.component';
     BrowserModule,
     HttpModule,
     SharedModule.forRoot(),
-    RouterModule.forRoot(ROUTES),
+    RouterModule.forRoot(ROUTES, {preloadingStrategy: PreloadAllModules}),
   ],
   providers: [
     {provide: LOCALE_ID, useValue: 'pt-BR'}],
