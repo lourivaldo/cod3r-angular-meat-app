a05bd5e92c9b3ffb7a22a895c298640117689b3d
Lourivaldo Vasconcelos <lourivaldo@mangue3.com.br>
2020-04-01 15:14:00 -0300

    animations

--- out-tsc/app/app/restaurant-detail/shopping-cart/shopping-cart.component.js
+++ out-tsc/app/app/restaurant-detail/shopping-cart/shopping-cart.component.js
@@ -9,9 +9,11 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 import { Component } from '@angular/core';
 import { ShoppingCartService } from './shopping-cart.service';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 var ShoppingCartComponent = (function () {
     function ShoppingCartComponent(shoppingCartService) {
         this.shoppingCartService = shoppingCartService;
+        this.rowState = 'ready';
     }
     ShoppingCartComponent.prototype.ngOnInit = function () {
     };
@@ -36,6 +38,21 @@ ShoppingCartComponent = __decorate([
     Component({
         selector: 'mt-shopping-cart',
         templateUrl: './shopping-cart.component.html',
+        animations: [
+            trigger('row', [
+                state('ready', style({ opacity: 1 })),
+                transition('void => ready', animate('300ms 0s ease-in', keyframes([
+                    style({ opacity: 0, transform: 'translateX(-30px)', offset: 0 }),
+                    style({ opacity: 0.8, transform: 'translateX(10px)', offset: 0.8 }),
+                    style({ opacity: 1, transform: 'translateX(0px)', offset: 1 }),
+                ]))),
+                transition('ready => void', animate('300ms 0s ease-in', keyframes([
+                    style({ opacity: 1, transform: 'translateX(0px)', offset: 0 }),
+                    style({ opacity: 0.8, transform: 'translateX(-10px)', offset: 0.2 }),
+                    style({ opacity: 0, transform: 'translateX(30px)', offset: 1 }),
+                ])))
+            ])
+        ]
     }),
     __metadata("design:paramtypes", [ShoppingCartService])
 ], ShoppingCartComponent);
--- src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
+++ src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
@@ -2,12 +2,30 @@ import { Component, OnInit } from '@angular/core';
 import {ShoppingCartService} from './shopping-cart.service';
 import {CartItem} from './cart-item.model';
 import {MenuItem} from '../menu-item/menu-item.model';
+import {trigger, state, style, transition, animate, keyframes} from '@angular/animations';
 
 @Component({
   selector: 'mt-shopping-cart',
   templateUrl: './shopping-cart.component.html',
+  animations: [
+    trigger('row', [
+      state('ready', style({opacity: 1})),
+      transition('void => ready', animate('300ms 0s ease-in', keyframes([
+        style({opacity: 0, transform: 'translateX(-30px)', offset: 0}),
+        style({opacity: 0.8, transform: 'translateX(10px)', offset: 0.8}),
+        style({opacity: 1, transform: 'translateX(0px)', offset: 1}),
+      ]))),
+      transition('ready => void', animate('300ms 0s ease-in', keyframes([
+        style({opacity: 1, transform: 'translateX(0px)', offset: 0}),
+        style({opacity: 0.8, transform: 'translateX(-10px)', offset: 0.2}),
+        style({opacity: 0, transform: 'translateX(30px)', offset: 1}),
+      ])))
+    ])
+  ]
 })
 export class ShoppingCartComponent implements OnInit {
+
+  rowState = 'ready';
 
   constructor(private shoppingCartService: ShoppingCartService) { }
 
